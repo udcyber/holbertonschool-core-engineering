c64a9302fe55c5ea868772dff492b72d4fc9d422
udcyber <udewaleyne.pro@gmail.com>
2026-05-05 10:10:34 +0200

    print lines

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+# Define variable
 language = ("Python")
 version = (3)
 pi = (3.14159265358979)
 computation = (1 > 0)
 
+# Output info
 print(f"Language: {language}")
 print("Version:", version)
 print(f"Pi approx: {pi: .2f}")
