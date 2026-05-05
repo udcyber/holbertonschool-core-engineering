e96e1541c0abe9f44fdfd1ee081df6805cce56f2
udcyber <udewaleyne.pro@gmail.com>
2026-05-05 10:08:32 +0200

    print lines

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-language = 'Python'
-version = 3
-pi = 3.14
+language = ("Python")
+version = (3)
+pi = (3.14159265358979)
 computation = bool(1)
 
-print(f'Language: {language}')
-print('Version:', version)
-print(f'Pi approx: {pi:.2f}')
+print(f"Language: {language}")
+print("Version:", version)
+print(f"Pi approx: {pi: .2f}")
 print('Computation valid:', computation)
