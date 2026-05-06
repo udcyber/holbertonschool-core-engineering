27186c570b8f913f013cd00571b613e4b363634a
udcyber <udewaleyne.pro@gmail.com>
2026-05-06 12:17:54 +0200

    change places for ord()

--- python_fundamentals/functions_modules/islower.py
+++ python_fundamentals/functions_modules/islower.py
@@ -6,7 +6,19 @@
 
 # Given Function
 def islower(c):
-    if c >= 97 and c <= 122:
+    """Check if c is a lowercase letter"""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
+
+
+# Tests
+print(islower('a'))
+print(islower('e'))
+print(islower('r'))
+print(islower('z'))
+print(islower('A'))
+print(islower('7'))
+print(islower('€'))
+print(islower(' '))
