af385802225ab8c75d65f01543ab199a8deedd03
udcyber <udewaleyne.pro@gmail.com>
2026-05-07 13:19:51 +0200

    print last digit

---
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Print the last digit of number
+# Return: the value of the last digit
+
+def print_last_digit(number):
+    print(abs(number) % 10)
+
+
+print(print_last_digit(-1555))
