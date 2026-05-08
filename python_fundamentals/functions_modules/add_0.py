c603bdbd20153d2c44a6201ef97adc6201a47bcc
udcyber <udewaleyne.pro@gmail.com>
2026-05-08 15:55:39 +0200

    import function add() from add_0.py and print result of addition

---
+++ python_fundamentals/functions_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def add(a, b):
+    """add a to b
+
+    Arguments:
+        a: 1st integer
+        b: 2nd integer
+
+    Return:
+        a + b
+    """
+    return (a + b)
