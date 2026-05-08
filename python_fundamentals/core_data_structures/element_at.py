14788ba835325c4b9259a958e0743b09dcd9fe77
udcyber <udewaleyne.pro@gmail.com>
2026-05-08 16:45:52 +0200

    retrieve an element from a list

--- python_fundamentals/core_data_structures/element_at.py
+++ python_fundamentals/core_data_structures/element_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def element_at(my_list, idx):
-    if idx >= 0 and idx < len(my_list):
-        return (my_list(idx))
+    """retrieve an element from a list"""
+    if idx > 0 and idx < len(my_list):
+        return (my_list[idx])
     else:
         return (None)
