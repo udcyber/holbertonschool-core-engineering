2d57490c8b34c1b92403641a8acf9ab876c84266
udcyber <udewaleyne.pro@gmail.com>
2026-05-08 17:45:58 +0200

    return result value of a raised to power of b

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-# Output info
 def pow(a, b):
     """return the result value of a raised to the power of b"""
     result = 1
     # _ here means "the loop counter is not used"
     for _ in range(b):
         result *= a
-    return result
+
+    return (result)
