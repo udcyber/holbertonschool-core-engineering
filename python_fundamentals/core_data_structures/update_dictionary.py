5be9dabdd91fa13e7cf846b1e67eed70aab377d9
udcyber <udewaleyne.pro@gmail.com>
2026-05-08 17:35:17 +0200

    replace or add a key/value pair in a dictionary

---
+++ python_fundamentals/core_data_structures/update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def update_dictionary(a_dictionary, key, value):
+    """replace or add a key/value pair in a ditionary"""
+    a_dictionary[key] = value
+
+    return (a_dictionary)
