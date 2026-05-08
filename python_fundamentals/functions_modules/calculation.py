573d9cdd4d87da8ddc9603b0bb026c79e696ecf8
udcyber <udewaleyne.pro@gmail.com>
2026-05-08 16:08:58 +0200

    removed description

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-    """print the result of:
-
-    The 4 Rules of Number:
-    - addition
-    - subtraction
-    - multiplication
-    - division
-
-    Arguments:
-    a = 1st integer
-    b = 2nd integer
-
-    Return:
-    the result value
-    """
     from calculator_1 import add, sub, mul, div
 
     a = 10
