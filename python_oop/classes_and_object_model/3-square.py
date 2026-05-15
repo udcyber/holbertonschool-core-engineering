db8adc592b48aab7095d9df8939913b5937c7cf0
udcyber <udewaleyne.pro@gmail.com>
2026-05-15 09:53:56 +0200

    return area of square

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Add an instance methode [area(self)] to the [Square] class"""
+"""that returns the area of the square based on its' sides' length"""
+
+
+class Square:
+    """Define [Square]"""
+    def __init__(self, size=0):
+        """Initialize [Square] with [size]
+
+            Arguments:
+                size: the size of the square
+
+            Raises:
+                Typerror: size is not an integer
+                ValueError: size is negative
+        """
+        # raise exceptions
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        # size is private
+        self.__size = size
+
+
+def area(self):
+    """Return the area of the square"""
+    return self.__size * self.__size
