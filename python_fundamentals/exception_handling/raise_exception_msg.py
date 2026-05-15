4b70816a071818dbd7caba7f790791dcaa1e761c
udcyber <udewaleyne.pro@gmail.com>
2026-05-15 08:33:53 +0200

    RAISE a NameError with a custom message

--- python_fundamentals/exception_handling/raise_exception_msg.py
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def raise_exception_msg(message=""):
-    """Raise a NameError with a custome message."""
+    """Raise a NameError with a custom message."""
     raise NameError(message)
