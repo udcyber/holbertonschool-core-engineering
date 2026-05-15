82d51237b830af1a06a321a8f4cfb8df62b52a56
udcyber <udewaleyne.pro@gmail.com>
2026-05-15 12:25:03 +0200

    Rectangle inherits from BaseGeometry

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-BaseGeometry = __import__("base_geometry").BaseGeometry
 """Create a class [Rectangle] that inherits from [BaseGeometry]"""
 # [Rectangle]
 # represents a rectangle
 # is defined by its width and height
+
+
+BaseGeometry = __import__("base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
