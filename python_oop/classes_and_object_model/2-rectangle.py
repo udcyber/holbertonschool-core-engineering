f3f1d5b507808e3d9eda31245eb8bc49ed93a814
udcyber <udewaleyne.pro@gmail.com>
2026-05-15 12:00:19 +0200

    add area and perimeter to Rectangle

--- python_oop/classes_and_object_model/2-rectangle.py
+++ python_oop/classes_and_object_model/2-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         """Return the area of the rectangle"""
         return self.__width * self.__height
 
-    def perimet(self):
+    def perimeter(self):
         """Return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
