a1fb7657867cc01ac6a3608000cbcb3fd0239200
udcyber <udewaleyne.pro@gmail.com>
2026-05-15 12:12:52 +0200

    create a class with two methods

---
+++ python_oop/inheritance/base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Create a class [BaseGeometry] with two methodes"""
+# [BaseGeometry] includes
+# area(self) and
+# integer_validator(self, name, value)
+
+
+class BaseGeometry:
+    """Definition of [BaseGeometry]"""
+    # represents the area of a geometric shape
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    # validates that a value represents a valid positive integer
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
